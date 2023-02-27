2e29e50e0c78669f78c55914b31b1883bc11f5d8
vanoengenharia <vanoengenharia@protonmail.com>
2023-02-27 16:08:19 -0300

    Migrations n Seeders

---
+++ back-end/src/database/migrations/20230227182803-create-sales.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('sales', { 
+      id:{
+        type:Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,        
+       },
+      total_price: {
+        type: Sequelize.DECIMAL(9,2),
+        allowNull: false,
+      },
+      delivery_address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      delivery_number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      sale_date: {
+        type: Sequelize.DATETIME,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      seller_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('sales');
+  }
+};
