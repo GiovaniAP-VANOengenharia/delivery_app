9083aeb9b36d4dbb8f4cd9802d6e99ecd1750611
vanoengenharia <vanoengenharia@protonmail.com>
2023-02-28 15:33:11 -0300

    [Group 8] Get All Pruducts

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { userRouter } = require('./routes/user.route');
+const { productRouter } = require('./routes/product.route');
 
 const app = express();
 app.use(express.json());
 
 app.use('/login', userRouter);
+app.use('/products', productRouter);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
