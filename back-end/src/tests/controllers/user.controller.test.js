5bdc18d5ee2b572f2eb83d22b5c3e2abd736d717
Gabriel Scott <gabrielpcreis@gmail.com>
2023-03-01 16:59:08 -0300

    text: Testes da service de Users feitos

---
+++ back-end/src/tests/controllers/user.controller.test.js
@@ -0,0 +1,17 @@
+// const { expect } = require("chai");
+// const sinon = require("sinon");
+// const userService = require("../../api/services/user.service");
+
+
+
+// describe("Testes controller de users", function () {
+//     describe("Fazendo login", function () {
+//         sinon.stub(userService, "getLogin").resolves();
+//         afterEach(async function () {
+//             sinon.restore();
+//         });
+//         it('Não é possivel realizar o login sem informar o email', async function () {
+//             const result =
+//         });
+//     });
+// });
---
+++ back-end/src/tests/services/mocks.user.service.mocks.js
@@ -0,0 +1,23 @@
+// const validUserInput = {
+//     email: 'fulana@deliveryapp.com',
+//     password: 'fulana@123'
+// };
+const validEmail = 'fulana@deliveryapp.com';
+const validPassword = 'fulana@123';
+const validUser = {
+    id: 2,
+    name: 'Fulana Pereira',
+    email: 'fulana@deliveryapp.com',
+    password: '3c28d2b0881bf46457a853e0b07531c6',
+    role: 'seller'
+};
+const validToken = {
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZW1haWwiOiJmZWxjaWFjbW9yYWVzX2JhdGlzdGFAaG90bWFpbC5jb20iLCJyb2xlIjoiY3VzdG9tZXIiLCJpYXQiOjE2Nzc2OTgzMTksImV4cCI6MTY3NzcwMTMxOX0.1o6TzgAzWKFtb5OSf-fA3vwMrc0Qw5_sHIuCDjTvwgY"
+};
+
+module.exports = {
+    validEmail,
+    validPassword,
+    validUser,
+    validToken
+};
