8afe18af47b3e83a5808c76cbf5b127941ebbe33
vanoengenharia <vanoengenharia@protonmail.com>
2023-03-02 20:29:19 -0300

    [ Group 8 ] Products LocalStorage

--- front-end/src/Components/ProductCard.jsx
+++ front-end/src/Components/ProductCard.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 function ProductCard(props) {
+  const [quantity, setQuantity] = useState(0);
   const { productData } = props;
   const { id, name, price, urlImage } = productData;
+
+  const inputQuantity = (e) => {
+    if (e.target.name === 'add') {
+      setQuantity(quantity + 1);
+    } else if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const checkLocalStorage = (products) => {
+    let cart = JSON.parse(products);
+    console.log(cart);
+    const findItem = cart.find((product) => product.id === id);
+    if (findItem && quantity > 0) {
+      const newItem = { ...findItem, quantity };
+      const filterCart = cart.filter((product) => product.id !== id);
+      cart = [...filterCart, newItem];
+    } else if (quantity > 0) {
+      cart.push({ id, name, price, quantity });
+    }
+    if (!quantity) {
+      const filterCart = cart.filter((product) => product.id !== id);
+      cart = [...filterCart];
+    }
+    const toCart = JSON.stringify(cart);
+    localStorage.setItem('products', toCart);
+  };
+
+  const toLocalStorage = () => {
+    const products = localStorage.getItem('products');
+    if (!products) {
+      const toCart = JSON.stringify([{ id, name, price, quantity }]);
+      localStorage.setItem('products', toCart);
+    } else {
+      checkLocalStorage(products);
+    }
+  };
+
+  useEffect(() => {
+    if (quantity < 0) setQuantity(0);
+    toLocalStorage();
+  }, [quantity]);
+
+  const inputChange = (e) => {
+    setQuantity(Number(e.target.value));
+  };
 
   return (
     <div>
@@ -24,18 +71,24 @@ function ProductCard(props) {
       <button
         type="button"
         data-testid={ `customer_products__button-card-add-item-${id}` }
+        name="add"
+        onClick={ (e) => inputQuantity(e) }
       >
         +
       </button>
 
       <input
         type="number"
+        value={ quantity }
         data-testid={ `customer_products__input-card-quantity-${id}` }
+        onChange={ (e) => inputChange(e) }
       />
 
       <button
         type="button"
         data-testid={ `customer_products__button-card-rm-item-${id}` }
+        name="sub"
+        onClick={ (e) => inputQuantity(e) }
       >
         -
       </button>
