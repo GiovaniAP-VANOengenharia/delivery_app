b3f1a89c11983afe8cb17754351ce40a77110eb2
Gabriel Scott <gabrielpcreis@gmail.com>
2023-03-02 14:42:22 -0300

    fix: primeiros testes da controller de usuários

--- back-end/src/tests/services/mocks/user.service.mocks.js
+++ back-end/src/tests/services/mocks/user.service.mocks.js
@@ -11,13 +11,9 @@ const validUser = {
     password: '3c28d2b0881bf46457a853e0b07531c6',
     role: 'seller'
 };
-const validToken = {
-    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZW1haWwiOiJmZWxjaWFjbW9yYWVzX2JhdGlzdGFAaG90bWFpbC5jb20iLCJyb2xlIjoiY3VzdG9tZXIiLCJpYXQiOjE2Nzc2OTgzMTksImV4cCI6MTY3NzcwMTMxOX0.1o6TzgAzWKFtb5OSf-fA3vwMrc0Qw5_sHIuCDjTvwgY"
-};
 
 module.exports = {
     validEmail,
     validPassword,
     validUser,
-    validToken
 };
