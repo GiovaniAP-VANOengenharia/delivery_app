2946d80a5ed289c245bb9532be50fe6b87ec91b6
GabrielAgrela <gabriel.agrela@hotmail.com>
2023-03-08 15:46:31 -0300

    feat: Criando css da tela de produtos

--- front-end/src/Pages/Products.jsx
+++ front-end/src/Pages/Products.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import NavBar from '../Components/NavBar';
 import ProductCard from '../Components/ProductCard';
 import { requestData } from '../services/requests';
@@ -16,16 +17,26 @@ function Products() {
   }, []);
 
   return (
-    <>
+    <div>
       <NavBar />
-      { productsArray.map((product) => (
-        <ProductCard productData={ product } key={ product.id } />
-      ))}
+      <ProductsContainer>
+        { productsArray.map((product) => (
+          <ProductCard productData={ product } key={ product.id } />
+        ))}
+      </ProductsContainer>
       <div>
         Ver Carrinho: R$ !valorTotal!
       </div>
-    </>
+    </div>
   );
 }
+
+const ProductsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: row wrap;
+  margin: 50px;
+`;
 
 export default Products;
